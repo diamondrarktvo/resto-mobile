8a918ba909d9c95e964fb3ed804d352c790a7a54
Diamondra RAKOTOARINAIVO <amada.rktvo@gmail.com>
2022-08-09 17:51:05 +0300

    Mise en place base project resto avec redux typescript avec prettier config, babel for env ignoreWarning for deprecated View , config tsconfig for path import

--- front/App.tsx
+++ front/App.tsx
@@ -1,24 +1,24 @@
+import './ignoreWarning';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-        <View style={styles.container}>
-        <Text>Bienvenu chez Resto mobile</Text>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaProvider>
-    
-  );
+   return (
+      <SafeAreaProvider>
+         <View style={styles.container}>
+            <Text>Bienvenu chez Resto mobile</Text>
+            <StatusBar style="auto" />
+         </View>
+      </SafeAreaProvider>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
 });
